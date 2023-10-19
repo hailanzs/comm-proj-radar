ff88234e6585e39e33373a10196f865d59665404
hash <hailan.shanbhag@epfl.ch>
2023-10-19 11:03:37 +0200

    comments added

--- scripts/1843_record.lua
+++ scripts/1843_record.lua
@@ -1,9 +1,11 @@
 -- capture_directory               =   "0616"
 capture_file                    =   "2"
 
---TODO: edit this path!!
+--TODO: edit this path!! (to where you want to save it)
 SAVE_DATA_PATH = "C:\\Users\\robin\\Documents\\Communication\\comm-proj-radar\\record\\" .. capture_file .. ".bin"
 
+-- this line configures where to save the data: it saves it to SAVE_DATA_PATH in a *.bin
 ar1.CaptureCardConfig_StartRecord(SAVE_DATA_PATH, 1)
+-- this line starts the radar transmitting
 ar1.StartFrame()
 
