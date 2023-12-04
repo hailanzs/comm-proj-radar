30d0bab972980df222439c1c649a50ed5c808f9a
hash <hailan.shanbhag@epfl.ch>
2023-12-04 19:59:06 +0100

    bf added

--- processing/_process_data.py
+++ processing/_process_data.py
@@ -1,9 +1,22 @@
 import numpy as np
 import scipy
 import scipy.io as sio
+from scipy import signal
 
 
 ### start helper functions ###
+
+
+def butter_highpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
+def butter_highpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_highpass(cutoff, fs, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
+
 
 def beamform(X, locs):
     """
